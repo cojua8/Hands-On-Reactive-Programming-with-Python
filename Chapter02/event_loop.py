eaac1f0b631706d3e33205b5c1f101979c83a624
Joaquín Neira <joaquin.neiraj@gmail.com>
2023-02-11 01:35:01 -0300

    formatting

--- Chapter02/event_loop.py
+++ Chapter02/event_loop.py
@@ -1,13 +1,15 @@
 import datetime
 import asyncio
 
+
 async def wait(delay):
     now = datetime.datetime.now()
-    print("wait for {} seconds at {}:{}:{}".format(delay, now.hour, now.minute, now.second))
+    print(f"wait for {delay} seconds at {now.strftime('%H:%M:%S')}")
     await asyncio.sleep(delay)
     now = datetime.datetime.now()
-    print("waited for {} seconds at {}:{}:{}".format(delay, now.hour, now.minute, now.second))
+    print(f"waited for {delay} seconds at {now.strftime('%H:%M:%S')}")
     return True
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(wait(2))
--- Chapter02/http_echo.py
+++ Chapter02/http_echo.py
@@ -1,18 +1,21 @@
 import asyncio
 from aiohttp import web
 
+
 async def echo_handler(request):
-    response = web.Response(text=request.match_info['what'])
+    response = web.Response(text=request.match_info["what"])
     await response.prepare(request)
     return response
 
+
 async def start_server(runner):
     await runner.setup()
-    site = web.TCPSite(runner, 'localhost', 8080)
+    site = web.TCPSite(runner, "localhost", 8080)
     await site.start()
 
+
 app = web.Application()
-app.router.add_route('GET', '/echo/{what}', echo_handler)
+app.router.add_route("GET", "/echo/{what}", echo_handler)
 runner = web.AppRunner(app)
 
 loop = asyncio.get_event_loop()
