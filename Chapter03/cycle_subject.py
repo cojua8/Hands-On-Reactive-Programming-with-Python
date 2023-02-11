13d0539d832e1fae3cd29aaab3df56054d321ff5
Joaquín Neira <joaquin.neiraj@gmail.com>
2023-02-11 01:35:21 -0300

    formatting and comments

--- Chapter03/cycle_subject.py
+++ Chapter03/cycle_subject.py
@@ -1,24 +1,34 @@
 import rx
 import rx.operators as ops
-from rx.subject import Subject
+from rx.subject.subject import Subject
+from rx.core.observer.observer import Observer
 
 
-def component_a(input):
-    return input.pipe(
-        ops.map(lambda i: i*2)
-    )
+# recibe un observable y devuelve otro observable
+def component_a(observable):
+    return observable.pipe(ops.map(lambda i: i * 2))
 
 
-def component_b(input):
-    input.subscribe(
+# recibe un observable y devuelve otro observable
+def component_b(observable):
+    observer = Observer(
         on_next=lambda i: print("item: {}".format(i)),
         on_error=lambda e: print("error: {}".format(e)),
-        on_completed=lambda: print("completed")
+        on_completed=lambda: print("completed"),
     )
-    return rx.from_([1, 2, 3])
+
+    observable.subscribe(observer)
+    return rx.from_([1, 2, 3])  # este es el punto de partida
 
 
-b_in_proxy = Subject()
-b_out = component_b(b_in_proxy)
-a_out = component_a(b_out)
-a_out.subscribe(b_in_proxy)
+proxy = Subject()  # el proxy actua como observador y como observable
+b_out = component_b(proxy)  # aqui proxy es el observable
+a_out = component_a(b_out)  # aqui se conectan los dos componentes
+a_out.subscribe(proxy)  # aqui proxy es el observador
+
+# no importa quien se conecte primero, el programa funciona al ejecutar la
+# subscripcion
+a_in_proxy = Subject()
+a_out = component_a(a_in_proxy)
+b_out = component_b(a_out)
+b_out.subscribe(a_in_proxy)
