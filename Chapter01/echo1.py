c59222e9632cec785456cc965c8d238e14493f6d
Joaquín Neira <joaquin.neiraj@gmail.com>
2023-02-11 01:01:41 -0300

    comentarios del cap 1

--- Chapter01/echo1.py
+++ Chapter01/echo1.py
@@ -1,9 +1,12 @@
 import sys
 import rx
 
+# crea un observable a partir de una lista, en este caso la lista de agumentos
 argv = rx.from_(sys.argv[1:])
 
+# se subscribe al observable para recibir los eventos
 argv.subscribe(
     on_next=lambda i: print("on_next: {}".format(i)),
     on_error=lambda e: print("on_error: {}".format(e)),
-    on_completed=lambda: print("on_completed"))
+    on_completed=lambda: print("on_completed"),
+)
--- Chapter01/echo2.py
+++ Chapter01/echo2.py
@@ -2,13 +2,15 @@ import sys
 import rx
 import rx.operators as ops
 
+
 argv = rx.from_(sys.argv[1:])
-argv = argv.pipe(
-    ops.map(lambda i: i.capitalize())
-)
+
+# le aplica la funcion capitalize a cada elemento del observable
+argv = argv.pipe(ops.map(lambda i: i.capitalize()))
 
 argv.subscribe(
     on_next=lambda i: print("on_next: {}".format(i)),
     on_error=lambda e: print("on_error: {}".format(e)),
-    on_completed=lambda: print("on_completed"))
+    on_completed=lambda: print("on_completed"),
+)
 print("done")
